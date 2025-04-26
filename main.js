f7dbe854c99b97a95567761b37a5104b4340065c
trezhywinks <dreqxyonion@gmail.com>
2025-04-26 02:53:53 +0000

    upgrade (Server TryZhy)

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = "9595";
 
+app.use(cors());
 app.use(express.static('./server'));
 
 app.listen(port, () => {
