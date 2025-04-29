d176e88430e21203ae04dfa39fd7b9d455b43547
trezhywinks <dreqxyonion@gmail.com>
2025-04-29 16:53:55 +0000

    👾 iberver _main.js

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
 const express = require('express');
+const figlet = require('figlet');
+const colors = require('colors');
 const cors = require('cors');
 const app = express();
 const port = "9595";
-
+const haki = "ibserver";
 app.use(cors());
 app.use(express.static('./server'));
 
+app.get('/u', (req, res) => {
+res.sendFile(__dirname + '/u/index.html')
+})
+
 app.listen(port, () => {
-console.log('http://localhost:9595')
+figlet.text(haki, {font: 'Small'}, (err, data) =>{
+if (err){
+console.log(err);
+}
+console.log(data.bold.yellow);
+console.log("GET".bold.yellow,"http://localhost:9595".bold.green)
+console.log("GET".bold.yellow,"http://localhost:9595/admin".bold.red)
+console.log("GET".bold.yellow,"http://localhost:9595/".bold.green + "u".bold.magenta)
+
+})
 })
