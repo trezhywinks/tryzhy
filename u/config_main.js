0ed60fedc76e2196b8d9d082ee65c01e8f8fca95
trezhywinks <dreqxyonion@gmail.com>
2025-04-30 21:15:17 +0000

    👾 bots server upgrade

--- u/config_main.js
+++ u/config_main.js
@@ -1,4 +1,6 @@
 document.querySelector('.back').addEventListener('click', () => {
-document.querySelector('.removet').style.display = "inline";
+const rick = document.querySelector('.removet')
+rick.style.display = "inline";
+rick.style.opacity = "1";
 document.querySelector('.settingsf').style.display = "none";
 });
