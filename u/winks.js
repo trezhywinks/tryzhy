72a30c243a5b2cac26436631f2f4cfec4b98ec10
trezhywinks <dreqxyonion@gmail.com>
2025-05-01 02:06:15 +0000

    👾 script winks

---
+++ u/winks.js
@@ -0,0 +1,7 @@
+document.querySelector('.servers').addEventListener('click', () =>{
+const reng = document.querySelector('.recad_o');
+reng.style.display = "none";
+const rewinks = document.querySelector('.wine_winks');
+rewinks.style.display = "inline";
+
+});
