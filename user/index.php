dbf37e17b747e0605c8df43f663ae37c9bbaa28a
trezhywinks <dreqxyonion@gmail.com>
2025-05-04 19:38:17 +0000

    commit wine winks

---
+++ user/index.php
@@ -0,0 +1,19 @@
+<?php
+$admin = 'Hello user!';
+?>
+<style>
+body{
+font-family: "SF Pro Text", "SF Pro Icons", system, -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", "Helvetica Neue", "Helvetica", "Arial", "Lucida Grande", "Ubuntu", "Cantarell", "Fira Sans", sans-serif;
+font-style: normal; font-weight: 400;
+background: #f7f7f7;
+color: #000000;
+}
+</style>
+<html><body>
+<center>
+<img src="https://trezhy.onrender.com/imagens/68110e02fb074f380ffb1e1d.png" style="border-radius:  3px; width: 120; height: 120;"/>
+<h2>
+<?php echo $admin;?><br>
+</center>
+</body>
+</html>
