2743d80769c97febcb2736ea46ae4f46138d04e2
trezhywinks <dreqxyonion@gmail.com>
2025-06-01 18:39:37 -0300

    update in search

--- u/winks.js
+++ u/winks.js
@@ -6,6 +6,7 @@ document.querySelector('.iframe_youmigmig').style.display = "none";
 rewinks.style.display = "inline";
 });
 
+
 document.getElementById('youmigmig').addEventListener('click', () =>{
 const gim = document.querySelector('.recad_o');
 gim.style.display = "none";
@@ -13,3 +14,5 @@ const iframe = document.querySelector('.iframe_youmigmig');
 document.querySelector('.wine_winks').style.display = "none";
 iframe.style.display = "inline";
 });
+
+
